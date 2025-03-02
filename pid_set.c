b6e75f7a99acfc0d48978f6fea8b02d1e6c73602
NikoB15 <130003251+NikoB15@users.noreply.github.com>
2025-03-02 01:14:07 +0000

    Make functions return bool

--- pid_set.c
+++ pid_set.c
@@ -1,29 +1,29 @@
 #include "pid_set.h"
 
-int init_pid_set(pid_set *set, size_t initial_capacity) {
+void init_pid_set(pid_set *set, size_t initial_capacity) {
     set->array = malloc(initial_capacity * sizeof(pid_t));
     set->capacity = initial_capacity;
     set->size = 0;
-    return 1;
 }
 
-int add_pid_set(pid_set *set, pid_t pid) {
+bool add_pid_set(pid_set *set, pid_t pid) {
     // Check whether element is already present
     for (size_t i = 0; i < set->size; i++) {
-        if (set->array[i] == pid) return 0;
+        if (set->array[i] == pid) return false;
     }
     
     // Add element
     set->size++;
-    if (set->size == set->capacity) {
+    if (set->size > set->capacity) {
         set->capacity *= 2;
         set->array = realloc(set->array, set->capacity * sizeof(pid_t));
     }
     set->array[set->size-1] = pid;
-    return 1;
+
+    return true;
 }
 
-int remove_pid_set(pid_set *set, pid_t pid) {
+bool remove_pid_set(pid_set *set, pid_t pid) {
     // Try to find element
     for (size_t i = 0; i < set->size; i++) {
         if (set->array[i] != pid) continue;
@@ -31,19 +31,19 @@ int remove_pid_set(pid_set *set, pid_t pid) {
         // Remove element
         set->array[i] = set->array[set->size-1];
         set->size--;
-        return 1;
+        return true;
     }
 
     // Element does not exist
-    return 0;
+    return false;
 }
 
-int free_pid_set(pid_set *set) {
-    if (set == NULL) return 0;
+bool free_pid_set(pid_set *set) {
+    if (set == NULL) return false;
 
     free(set->array);
     set->array = NULL;
     set->capacity = 0;
     set->size = 0;
-    return 1;
+    return true;
 }
--- pid_set.h
+++ pid_set.h
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct {
     pid_t *array;
@@ -7,7 +8,7 @@ typedef struct {
     size_t size;
 } pid_set;
 
-int init_pid_set(pid_set *set, size_t initial_capacity);
-int add_pid_set(pid_set *set, pid_t pid);
-int remove_pid_set(pid_set *set, pid_t pid);
-int free_pid_set(pid_set *set);
+void init_pid_set(pid_set *set, size_t initial_capacity);
+bool add_pid_set(pid_set *set, pid_t pid);
+bool remove_pid_set(pid_set *set, pid_t pid);
+bool free_pid_set(pid_set *set);
