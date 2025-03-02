fdbd00d7107b8b0509c86ba7b15139a3d061787b
NikoB15 <130003251+NikoB15@users.noreply.github.com>
2025-03-02 01:15:56 +0000

    Add header file

--- parser.c
+++ parser.c
@@ -1,27 +1,4 @@
-#define _XOPEN_SOURCE 700  // Prevent sigaction struct linting error
-
-#include <fcntl.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include "pid_set.h"
-
-#define INPUT_LENGTH 2048
-#define MAX_ARGS 512
-
-typedef struct {
-    char *argv[MAX_ARGS + 1];
-    int argc;
-    char *input_file;
-    char *output_file;
-    bool is_bg;
-} command_line;
+#include "parser.h"
 
 static bool is_foreground_only = false;
 
